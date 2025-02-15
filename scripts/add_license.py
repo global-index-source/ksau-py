e5f4f596969b2efa0d3f1053a2c0a6cc27b86d49
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2025-02-15 23:31:24 +0800

    chore: add license header

--- scripts/add_license.py
+++ scripts/add_license.py
@@ -4,7 +4,7 @@ from pathlib import Path
 license_header = """\
 # SPDX-License-Identifier: Apache-2.0
 #
-# Copyright 2025 Firdaus Hakimi <hakimifirdaus944@gmail.com>
+# Copyright 2025 Global Index Source developers
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- src/ksau_py/__init__.py
+++ src/ksau_py/__init__.py
@@ -1,2 +1,22 @@
-def main() -> None:
-    print("Hello from ksau-py!")
+# SPDX-License-Identifier: Apache-2.0
+#
+# Copyright 2025 Global Index Source developers
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import asyncio
+
+from typer import Typer
+
+event_loop = asyncio.new_event_loop()
+app: Typer = Typer(name="ksau-py")
